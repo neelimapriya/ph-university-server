8cf3ec7b67e54609e1bc8b592adca995419a66e0
Nelima sultana <neelimasultana6@gmail.com>
2024-12-05 20:59:35 +0600

    add pre for create and update academic department

--- src/app/module/academicDepartment/academicDepartment.model.ts
+++ src/app/module/academicDepartment/academicDepartment.model.ts
@@ -1,22 +1,45 @@
-import { model, Schema } from "mongoose";
-import { TAcademicDepartment } from "./academicDepartment.interface";
+import { model, Schema } from 'mongoose';
+import { TAcademicDepartment } from './academicDepartment.interface';
 
-const academicDepartmentSchema=new Schema<TAcademicDepartment>({
-    name:{
-        type:String,
-        required:true,
-        unique:true
+const academicDepartmentSchema = new Schema<TAcademicDepartment>(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    academicFaculty:{
-        type:Schema.Types.ObjectId,
-        ref:'AcademicFaculty'
-    }
-},
-{
-    timestamps:true
+    academicFaculty: {
+      type: Schema.Types.ObjectId,
+      ref: 'AcademicFaculty',
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+academicDepartmentSchema.pre('save', async function (next) {
+  const isDepartmentExist = await AcademicDepartmentModel.findOne({
+    name: this.name,
+  });
+  if (isDepartmentExist) {
+    throw new Error('this department is already exist!');
+  }
+  next();
+});
+
+academicDepartmentSchema.pre('findOneAndUpdate',async function(next){
+    const query=this.getQuery()
+    console.log(query)
+    const isDepartmentExist = await AcademicDepartmentModel.findOne({query});
+    console.log(isDepartmentExist)
+      if (!isDepartmentExist) {
+        throw new Error('this department does not exist!');
+      }
+      next();
 })
 
-export const AcademicDepartmentModel=model<TAcademicDepartment>(
-    'AcademicDepartment',
-    academicDepartmentSchema
-)
+export const AcademicDepartmentModel = model<TAcademicDepartment>(
+  'AcademicDepartment',
+  academicDepartmentSchema,
+);
