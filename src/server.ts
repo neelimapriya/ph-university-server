cbc9c0b7e03887f63224417a5723772f73954428
Nelima sultana <neelimasultana6@gmail.com>
2024-12-09 21:11:15 +0600

    add function for closing the server

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,9 @@
 import app from './app';
 import config from './app/config';
-
+import { Server } from 'http';
 import mongoose from 'mongoose';
 
+let server:Server
 async function main() {
   try {
     await mongoose.connect(config.DATABASE_URL as string);
@@ -15,3 +16,18 @@ async function main() {
   }
 }
 main();
+
+process.on('unhandledRejection',()=>{
+  console.log(`❌unhandledRejection is detected, shutting down ........`)
+  if(server){
+    server.close(()=>{
+      process.exit(1)
+    })
+  }
+  process.exit(1)
+})
+
+process.on('uncaughtException',()=>{
+  console.log(`☠️ uncaughtException is detected,shutting down..`)
+  process.exit(1)
+})
